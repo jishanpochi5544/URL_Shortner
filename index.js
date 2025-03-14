645c931b68e99976abc899a63b12b460dfffba6b
Jishan Pochi <jishan.pce22@sot.pdpu.ac.in>
2025-03-14 06:33:21 +0530

    Fixed table rendering issue and improved URL shortener functionality

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const {connectToMongoDb} = require('./connect');
 const URL = require('./models/url')
 
-const urlRoute = require('./routes/url')
+const path = require("path")
 
+const urlRoute = require('./routes/url')
+const staticRoute = require('./routes/staticRouter')
 const app = express();
 const port = 8080;
 
@@ -11,10 +13,50 @@ connectToMongoDb('mongodb://127.0.0.1:27017/short-url')
 .then(() =>console.log("MongoDb Coonected....")
 );
 
+
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"))
+
+app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use('/url', urlRoute);
+app.get('/test',async (req,res)=>{
+    const allUrls = await URL.find({});
+    return res.render('home',{
+        urls:allUrls,
+    })
+})
 
-app.get('/:shortId', async (req,res)=>{
+/*
+app.get('/test',async (req,res)=>{
+    const allUrls = await URL.find({});
+    return res.render('home',{
+        urls:allUrls,
+    })
+    // return res.end(`
+    //     <html>
+    //     <head></head>
+    //     <body>
+    //         <ol>
+    //             ${allUrls
+    //                 .map(
+    //                     (url) =>
+    //                         `<li>${url.shortId} - ${url.redirectURL} - ${url.visitHistory.length}</li>`
+    //                 )
+    //                 .join("")
+    //             }
+    //         </ol>
+    //     </body>
+    //     </html>
+    //     `)
+    // return res.end('<h1> Hey from server</h1>');
+})
+
+*/
+
+app.use('/',staticRoute);
+
+app.get('/url/:shortId', async (req,res)=>{
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId
